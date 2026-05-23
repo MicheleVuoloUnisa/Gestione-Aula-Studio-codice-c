0ce75fa499d0f2777f3e45a2f89d3a551b6506e9
MicheleVuoloUnisa <m.vuolo14@studenti.unisa.it>
2026-05-23 21:19:33 +0200

    definizione dell'adt aula-studio. Abbiamo definito l'adt aula studio che rappresenta l'aula studio vera e propria.

---
+++ include/AulaStudio.h
@@ -0,0 +1,4 @@
+#include "utilitiesListaAttesa.h"
+int occupazionePosto(prenotazione p);
+int postiDisponibiliFasciaOraria(int inizioOra, int fineOra);
+int visualizzaStudentiPresenti(int inizioOra);
