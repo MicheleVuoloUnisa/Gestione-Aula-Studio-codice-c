508c40b41f10ac0b80273c1ada2a962ff4d8ad3e
MicheleVuoloUnisa <m.vuolo14@studenti.unisa.it>
2026-05-24 11:33:52 +0200

    Inserimento della funzione: Gestione check-in senza prenotazione. C'E' UN ERRORE: non riconosce studente, E' un errore calcolato dato che ancora dobbiamo includere tutte le librerie.

--- include/AulaStudio.h
+++ include/AulaStudio.h
@@ -2,3 +2,4 @@
 int occupazionePosto(prenotazione p);
 int postiDisponibiliFasciaOraria(int inizioOra, int fineOra);
 int visualizzaStudentiPresenti(int inizioOra);
+int gestioneIngressoSenzaPrenotazione(studente s, int inizioOra, int fineOra);
--- src/AulaStudio.c
+++ src/AulaStudio.c
@@ -11,6 +11,8 @@ l'indice 4 indica le fasce orarie (9-11;11-13;13-15;15-17)
 */
 static int AulaStudio[NUMPOSTI][FASCIAORARIA];
 
+
+//funzione check-in studente prenotato
 int occupazionePosto(prenotazione p){
     if(p==NULL){
         printf("prenotazione inesistente oppure errore di memoria");
@@ -72,4 +74,47 @@ int postiDisponibiliFasciaOraria(int inizioOra, int fineOra){
         }
     }
     return 0;
+}
+
+/*
+ * Funzione: gestioneIngressoSenzaPrenotazione
+ * * Descrizione:
+ * Gestisce l'ingresso "last-minute" di uno studente non prenotato.
+ * Verifica la disponibilità di un posto libero per la fascia oraria indicata
+ * e, in caso positivo, assegna il primo posto disponibile aggiornando lo stato dell'aula.
+ *
+ * Input:
+ * - s: Puntatore all'ADT studente che richiede l'accesso.
+ * - inizioOra: Intero che rappresenta l'ora di inizio della fascia oraria.
+ * - fineOra: Intero che rappresenta l'ora di fine della fascia oraria.
+ *
+ * Output:
+ * - Intero che rappresenta il numero del Posto assegnato (da 1 a NUMPOSTI).
+ * - Ritorna -1 se lo studente è NULL o se la fascia oraria non è valida.
+ * - Ritorna 0 se l'aula è completamente piena per quella fascia oraria.
+ *
+ * Pre-condizione:
+ * Lo studente 's' deve essere stato allocato e inizializzato correttamente.
+ *
+ * Post-condizione:
+ * Se c'è posto, la funzione restituisce il numero del posto assegnato.
+ * * Effetti Collaterali:
+ * Modifica lo stato della matrice globale AulaStudio, impostando a 1 la cella 
+ * corrispondente al posto trovato per occuparlo fisicamente.
+ */
+int gestioneIngressoSenzaPrenotazione(studente s, int inizioOra, int fineOra){
+    if(s==NULL){
+        return -1;
+    }
+    if(controlloFasciaOraria(inizioOra,fineOra)==-1){
+        return -1;
+    }
+    int tempFascia=mappaturaFasciaOraria(inizioOra);
+    for(int i=0;i<NUMPOSTI;i++){
+        if(AulaStudio[i][tempFascia]==0){
+            AulaStudio[i][tempFascia]=1; // OCCUPO IL POSTO (Check-in effettivo)
+            return i+1;// Restituisco il posto in formato umano (1-50)
+        }
+    }
+    return 0; //l'aula studio per quella fascia oraria è piena
 }
